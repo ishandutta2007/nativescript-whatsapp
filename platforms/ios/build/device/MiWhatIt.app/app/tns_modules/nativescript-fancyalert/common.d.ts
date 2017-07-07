c07f8257dcd0476049d43492e868135eb3bb5b3d
Ishan Dutta <ishandutta2007@gmail.com>
2017-07-07 21:27:47 +0530

    removing ignored files from repo

--- platforms/ios/build/device/MiWhatIt.app/app/tns_modules/nativescript-fancyalert/common.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class TNSFancyAlertButton {
-    label: string;
-    action: Function;
-    constructor(model?: any);
-}
--- platforms/ios/build/emulator/MiWhatIt.app/app/tns_modules/nativescript-geolocation/nativescript-geolocation-common.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Location = (function () {
-    function Location() {
-    }
-    return Location;
-}());
-exports.Location = Location;
-exports.defaultGetLocationTimeout = 5 * 60 * 1000;
